d2d44d925def4bc2384cbf1f350336612aaa11e0
jenko <callumclaytonjenkins148@outlook.com>
2023-04-27 10:41:18 +0100

    testing done

--- bootcamp-calendar/src/Components/App/App.test.js
+++ bootcamp-calendar/src/Components/App/App.test.js
@@ -1,37 +1,24 @@
-import {fireEvent, render, screen } from '@testing-library/react';
+import {userEvent, render, screen, waitFor, fireEvent } from '@testing-library/react';
 import { expect, test } from '@jest/globals';
-import Modal from '../Modal/Modal';
 import App from './App';
 
-/*
-
-test("Modal should appear when Monday header button is clicked", async function() {
-  render(<App />);
-
-  fireEvent.click(screen.getAllByRole('button', { class : "day-header-button" })[2]);
-
-  let dummyData = [{}];
-
-  //render(<Modal filteredData={dummyData} setModalToggle={jest.fn}/>);
-
-  const actual = screen.getByTestId('modalBackground');
-
-<<<<<<< HEAD
-  await waitFor(() => {
-    expect(actual).toBeInTheDocument();
-    // expect(screen.getByTestId('modalBackground')).toBeInTheDocument();
-  }
-  // expect(screen.getByTestId('modalBackground')).toBeInTheDocument();
-)});
 
 test.only("Modal should appear when day header button is clicked.", async function() {
-  render(<App />);  
-  //const btn = await screen.findByRole('button', { name: /monday/i});
-  screen.logTestingPlaygroundURL();
+  render(<App />);
+  
+  const btn = screen.getByRole('button', {
+    name: /Monday 24 \/ 4 resources/i
+  });
 
+  await waitFor(() => {
+    expect(btn).toBeVisible();
+  });
+
+  fireEvent.click(btn);
+
+  await waitFor(() => {
+    expect(screen.getByTestId('modalBackground')).toBeInTheDocument();
+  });
 });
-*/
-=======
-  expect(actual).toBeInTheDocument();
-}); 
->>>>>>> 6a21bde5893a2e2b95cb90a7d9f31cf0829ad219
+
+
