94aafd594aff035d8a2b9f5b8e9b6c7d8a964c23
jenko <callumclaytonjenkins148@outlook.com>
2023-04-27 11:35:34 +0100

    added hovering affect

--- bootcamp-calendar/src/Components/Footer/Footer.js
+++ bootcamp-calendar/src/Components/Footer/Footer.js
@@ -11,13 +11,13 @@ function Footer() {
   return (
     <footer className="footer-container">
         <div className="socials">
-            <a href="https://en-gb.facebook.com/schoolofcode/"><FontAwesomeIcon icon={faFacebook} /></a>
-            <a href="https://www.linkedin.com/school/school-of-code/"><FontAwesomeIcon icon={faLinkedin} /></a>
-            <a href="https://twitter.com/theSchoolOfCode"><FontAwesomeIcon icon={faTwitter} /></a>
-            <a href="https://slack.com/intl/en-gb/"><FontAwesomeIcon icon={faSlack} /></a>
+            <a href="https://en-gb.facebook.com/schoolofcode/"><FontAwesomeIcon className="hover" icon={faFacebook} /></a>
+            <a href="https://www.linkedin.com/school/school-of-code/"><FontAwesomeIcon className="hover" icon={faLinkedin} /></a>
+            <a href="https://twitter.com/theSchoolOfCode"><FontAwesomeIcon className="hover" icon={faTwitter} /></a>
+            <a href="https://slack.com/intl/en-gb/"><FontAwesomeIcon className="hover" icon={faSlack} /></a>
         </div>
         <div className="mail-icon">
-            <a href="https://www.schoolofcode.co.uk/"><FontAwesomeIcon icon={faEnvelope}/></a>
+            <a href="https://www.schoolofcode.co.uk/"><FontAwesomeIcon className="hover" icon={faEnvelope}/></a>
         </div> 
     </footer>
   )
