9ba48d5dfd64541a82205f6686042297ca23f818
MMoallin <Mohamed.moallin@outlook.com>
2023-04-27 10:43:07 +0100

    Target attribute to the links + edited the mail link

--- bootcamp-calendar/src/Components/Footer/Footer.js
+++ bootcamp-calendar/src/Components/Footer/Footer.js
@@ -11,13 +11,14 @@ function Footer() {
   return (
     <footer className="footer-container">
         <div className="socials">
-            <a href="https://en-gb.facebook.com/schoolofcode/"><FontAwesomeIcon icon={faFacebook} /></a>
-            <a href="https://www.linkedin.com/school/school-of-code/"><FontAwesomeIcon icon={faLinkedin} /></a>
-            <a href="https://twitter.com/theSchoolOfCode"><FontAwesomeIcon icon={faTwitter} /></a>
-            <a href="https://slack.com/intl/en-gb/"><FontAwesomeIcon icon={faSlack} /></a>
+        <a href="https://en-gb.facebook.com/schoolofcode/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faFacebook} /></a>
+        <a href="https://www.linkedin.com/school/school-of-code/" target="_blank "rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a>
+
+            <a href="https://twitter.com/theSchoolOfCode" target="_blank "rel="noopener noreferrer"><FontAwesomeIcon icon={faTwitter} /></a>
+            <a href="https://slack.com/intl/en-gb/" target="_blank "rel="noopener noreferrer"><FontAwesomeIcon icon={faSlack} /></a>
         </div>
         <div className="mail-icon">
-            <a href="https://www.schoolofcode.co.uk/"><FontAwesomeIcon icon={faEnvelope}/></a>
+        <a href="mailto:someone@example.com" target="_blank "rel="noopener noreferrer"><FontAwesomeIcon icon={faEnvelope}/></a>
         </div> 
     </footer>
   )
